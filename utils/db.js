06c8180f6e092c2b471a0e8c60cb35a72ab9ec02
Kaspar <kylgorvigajava@gmail.com>
2025-01-31 11:02:35 +0200

    Display all articles

--- utils/db.js
+++ utils/db.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if(err) throw err
-    console.log('Connected to MySQL')
+    console.log('MySQL CONNECTED')
 });
 
 module.exports = connection;
