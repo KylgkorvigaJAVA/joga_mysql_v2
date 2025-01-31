fea6fc5058b2801469bb398c504c2a454fbdd853
Kaspar <kylgorvigajava@gmail.com>
2025-01-31 11:06:52 +0200

    As a user i can see all yoga articles. See #1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./utils/app');
 const db = require('./utils/db');
+const articleRouter = require('./routes/articles');
+
+app.use('/', articleRouter);
 
 app.listen(3012, () => {
-    console.log('Server is running on port 3012');
+    console.log('Server RUNNING on localhost:3012');
 });
--- utils/app.js
+++ utils/app.js
@@ -3,13 +3,15 @@ const path = require('path');
 const hbs = require('express-handlebars');
 
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/../views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: path.join(__dirname, 'views/layouts')
+    layoutsDir: __dirname + '/../views/layouts/'
 }));
+
+app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 
