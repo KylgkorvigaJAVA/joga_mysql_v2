05436044eaea208368f04e179f2f120cd64e63e4
Kaspar <kylgorvigajava@gmail.com>
2025-02-14 13:28:48 +0200

    Created UI for submitting new article

--- utils/app.js
+++ utils/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
 const app = express();
+
 app.set('views', path.join(__dirname, '/../views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
@@ -12,7 +12,24 @@ app.engine('hbs', hbs.engine({
 }));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({extended: true}));
+app.get('/articles/new', (req, res) => {
+    res.render('articles/new');
+});
+
+app.post('/articles', (req, res) => {
+    const { name, published, author_name, body, image } = req.body;
+
+    const article = {
+        name,
+        published,
+        author_name,
+        image,
+        body
+    };
+
+    res.render('article', { article });
+});
 
 module.exports = app;
