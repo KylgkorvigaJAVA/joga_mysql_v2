1c86cf8437c23d0101a78b951428ac1d57d718dd
Kaarel <kaarel.viilvere@voco.ee>
2025-02-14 11:45:38 +0200

    Added post line in routes article.js file

--- routes/articles.js
+++ routes/articles.js
@@ -4,5 +4,5 @@ const articleController = require('../controllers/articles');
 
 router.get('/', articleController.getAllArticles);
 router.get('/article/:slug', articleController.getArticleBySlug);
-
+router.post('/');
 module.exports = router;
