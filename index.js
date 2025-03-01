a599c2b07a7f6fd08be1b337d47a0e29cbb8d417
Kaarel Viilvere <viilvere.k@gmail.com>
2025-03-01 13:07:21 +0200

    typo fix

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const articleRouter = require('./routes/articles');
 app.use('/', articleRouter);
 
 app.listen(3013, () => {
-    console.log('Server RUNNING on localhost:3012');
+    console.log('Server RUNNING on localhost:3013');
 });
--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12345',//qwerty
+    password: 'qwerty',
     database: 'joga_mysql'
 });
 
